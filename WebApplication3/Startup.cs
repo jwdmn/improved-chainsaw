5dd120e1368208d3f7921cef5866f894e73fe4b6
jonas wadman <wadmanjonas@gmail.com>
2017-12-30 14:23:43 +0100

    first commit

--- WebApplication3/Startup.cs
+++ WebApplication3/Startup.cs
@@ -9,26 +9,33 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace WebApplication3
 {
-    public class Startup
+  public class Startup
+  {
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
     }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+
+      app.UseStaticFiles();
+
+      //app.UseAuthentication();
+
+      app.UseMvc(cfg =>
+      {
+        cfg.MapRoute("Default",
+          "{controller}/{action}/{id?}",
+          new { controller = "App", Action = "Index" });
+      });
+    }
+  }
 }
